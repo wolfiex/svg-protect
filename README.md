4d9ab04f2362f158dc8e67005b368345014dae9b
Dan Ellis <daniel.ellis.research@googlemail.com>
2018-02-19 21:45:53 +0000

    release 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # svg-protect
-Illustrator/Publsher proof your svgs before downloading. 
+Illustrator/Publsher proof your svgs before downloading.
+=========
+
+## Installation
+
+  `npm install svg-protect`
+
+## Usage
+=========
+    const svgp = require('svg-protect');
+
+    var svg = document.getGetElementById('mysvg')
+
+    svggp(svg,{
+                  owner:"Dan Ellis <my_email@test.com>"
+                  flatten:true,
+                  elementids: "",
+                  split:false,
+                  cclicense:true,
+                  cclogo:true,
+                  filename:"test"// dont include '.svg'
+        })
+
+
+## Process
+This program takes several steps to attempt to protect an svg. These are:
+
+1. Flatten all group elements into main svg body.
+2. Optimise svg code and convert all elements into Paths.
+3. Rename all svg element ids to state ownership.   
+4. Split paths into triangles.
+5. Add CC Atrribution licence within svg comments.
+
+
+
+
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
---
+++ index.js
@@ -0,0 +1,185 @@
+"use strict";
+
+/*
+## Usage
+=========
+    const svgp = require('svg-protect');
+
+    var svg = document.getGetElementById('mysvg')
+
+    svggp(svg,{
+                  owner:"Dan Ellis <my_email@test.com>"
+                  flatten:true,
+                  elementids: "",
+                  split:false,
+                  cclicense:true,
+                  cclogo:true,
+                  filename:"test"// dont include '.svg'
+        })
+ */
+module.exports = function(svgDOM, opt) {
+  var SVGO = require("svgo");
+  var parse = require('parse-svg-path')
+  var contours = require('svg-path-contours')
+  var simplify = require('simplify-path')
+  var triangulate = require('triangulate-contours')
+
+
+  svgo = new SVGO({
+    plugins: [
+      {
+        convertShapeToPath: { convertArcs: true }
+      },
+      {
+        cleanupAttrs: true
+      },
+      {
+        removeDoctype: true
+      },
+      {
+        removeXMLProcInst: true
+      },
+      {
+        removeComments: true
+      },
+      {
+        removeMetadata: true
+      },
+      {
+        removeTitle: true
+      },
+      {
+        removeDesc: true
+      },
+      {
+        removeUselessDefs: true
+      },
+      {
+        removeEditorsNSData: true
+      },
+      {
+        removeEmptyAttrs: true
+      },
+      {
+        removeHiddenElems: true
+      },
+      {
+        removeEmptyText: true
+      },
+      {
+        removeEmptyContainers: true
+      },
+      {
+        removeViewBox: false
+      },
+      {
+        cleanUpEnableBackground: true
+      },
+      {
+        convertStyleToAttrs: true
+      },
+      {
+        convertColors: true
+      },
+      {
+        convertArcs: true
+      },
+      {
+        convertPathData: true
+      },
+      {
+        convertTransform: true
+      },
+      {
+        removeUnknownsAndDefaults: true
+      },
+      {
+        removeNonInheritableGroupAttrs: true
+      },
+      {
+        removeUselessStrokeAndFill: true
+      },
+      {
+        removeUnusedNS: true
+      },
+      {
+        cleanupIDs: true
+      },
+      {
+        convertShapeToPath: true
+      },
+      {
+        cleanupNumericValues: true
+      },
+      {
+        moveElemsAttrsToGroup: true
+      },
+      {
+        moveGroupAttrsToElems: true
+      },
+      {
+        collapseGroups: true
+      },
+      {
+        removeRasterImages: true
+      },
+      {
+        mergePaths: true
+      },
+      {
+        convertShapeToPath: true
+      },
+      {
+        sortAttrs: true
+      },
+      {
+        transformsWithOnePath: true
+      },
+      {
+        removeDimensions: true
+        //},{
+        //removeAttrs: {attrs: '(stroke|fill)'},
+      }
+    ]
+  });
+
+
+
+
+
+
+
+
+  function flatten (svg){
+    //remove groups
+    var gs = document.querySelectorAll('g')
+
+    gs.forEach( g=>{
+      styles = g.getAttribute('style')||'';
+      if(styles.trim().slice(-1)!=';'){styles+= ';'}
+      var children = g.children
+
+      d3.range(children.length).forEach(c=>{
+
+        var child = children[0] //these are lost when added to main svg, therefore only pick first
+        childstyle = child.getAttribute('style')||''
+        child.setAttribute('style',styles+childstyle)//ensure we are not missing this
+        child.id= window.copyright||'copyright Dan Ellis: ' + new Date()
+        svg.appendChild(child);
+
+      })
+
+  g.remove()
+
+    } )
+  svg.innerHTML=svg.innerHTML.trim()//rm whitespace
+  }
+
+
+
+
+if (opt.flatten){flatten(svgDOM)}
+
+return svg 
+
+};
